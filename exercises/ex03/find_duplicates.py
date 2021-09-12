39e09ddb5d38c50ae542bfa9676c14a6dc8a80f2
A'sjei Scott <cessura@live.unc.edu>
2021-09-12 19:12:55 -0400

    completed exercises

--- exercises/ex03/find_duplicates.py
+++ exercises/ex03/find_duplicates.py
@@ -1,3 +1,29 @@
 """Finding duplicate letters in a word."""
 
-__author__ = "123456789"
+__author__ = "730330727"
+
+comparator: str = str("")
+considered: str = str("")
+i: int = int(0)
+n: int = int(0)
+result: str = str("False")
+phrase: str = str (input("Enter a word: "))
+
+while i < len(phrase):
+    comparator = phrase[i]
+
+    while n < len(phrase):
+        if i == n:
+            n = n + 1
+
+        if n < len(phrase):
+            considered = phrase[n]
+
+        if considered == comparator:
+            result = "True"
+        n = n + 1
+
+    i = i + 1
+    n = 0
+
+print("Found duplicate: " + result)
--- exercises/ex03/tar_heels.py
+++ exercises/ex03/tar_heels.py
@@ -1,6 +1,21 @@
 """An exercise in remainders and boolean logic."""
 
-__author__ = "ENTER YOUR 9-DIGIT PID HERE"
+__author__ = "730330727"
 
 
 # Begin your solution here...
+    
+raci: str = str("CAROLINA")
+num: int = int(input("Enter an int: "))
+
+if num % 2 == 0: 
+    raci = "TAR"
+
+if num % 7 == 0:
+    raci = "HEEL"
+
+if num % 14 == 0:
+    raci = "TAR HEEL"
+
+print (raci)
+
